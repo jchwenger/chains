895176722023f19dfe2eda24020b296e11ae3917
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-04-06 19:31:21 +0100

    sketch | wip of transitionLeft & transitionRight logic

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -5,8 +5,10 @@ let lineIndex, totalLines;
 let currentTextSize;
 let currentLine;
 let processedLines;
-let widestChar, charZoomFactor;
+let charZoomFactor;
 let transitionLeft, transitionRight;
+let longestLength;
+let halfWidth;
 
 let initCursor;
 let currentShift;
@@ -32,6 +34,8 @@ function setup() {
   canvasSize = 500;
   createCanvas(canvasSize, canvasSize);
 
+  halfWidth = width/2;
+
   // remove last empty line if there is one
   lines = lines.filter(l => l.length > 0);
 
@@ -44,7 +48,6 @@ function setup() {
 
   currentTextSize = canvasSize/10;
   fill(0)
-    .strokeWeight(0)
     .textSize(currentTextSize);
 
   initCursor = new Cursor(mouseX, mouseY);
@@ -55,7 +58,7 @@ function setup() {
   currentShift = 0;
 
   // find the max charWidth of the current lines
-  widestChar = Array.from(new Set(lines.join("").split("")))
+  const widestChar = Array.from(new Set(lines.join("").split("")))
     .reduce((char1, char2) => textWidth(char1) > textWidth(char2) ? char1 : char2);
 
   // charWidth = canvasSize/18;
@@ -73,8 +76,12 @@ function setup() {
   processedLines = prepareLines();
   // console.log(processedLines);
 
-  transitionLeft = null;
-  transitionRight = - (processedLines[lineIndex + 1].ws * charWidth);
+  transitionLeft = processedLines[0].ws * charWidth;
+  transitionRight = processedLines[lineIndex + 1].ws * charWidth;
+
+  const lastGroup = processedLines[processedLines.length - 1].l;
+  longestLength = - (lastGroup[lastGroup.length - 1].length + 1) * charWidth + width - margins;
+  console.log(`longestLength: ${longestLength}`);
 
 }
 
@@ -103,23 +110,61 @@ function draw() {
 
 }
 
-function writeLine(line, h) {
+function writeLine(l, h) {
   push();
-  const cS = Math.min(0, currentShift + horizontalShift);
+  const cS = constrain(currentShift + horizontalShift, longestLength, 0);
   const vS = Math.min(0, verticalShift);
 
-  if (lineIndex < processedLines.length - 2) {
-    const transition = - (processedLines[lineIndex + 1].ws * charWidth);
-    console.log(`current horizontal shift: ${cS} | vertical shift: ${vS} | current ws: ${processedLines[lineIndex + 1].ws}, * char width: ${transition.toPrecision(6)}`);
-    if (cS < transition) {
-      lineIndex = (lineIndex + 1) % (processedLines.length - 1);
-      console.log("transition!");
+  const tr = cS + transitionRight;
+  const tl = cS + transitionLeft;
+
+  push();
+
+  textSize(15);
+  textAlign(LEFT);
+  stroke(255,0,0);
+
+  // helper: transition Right
+  line(tr, 0, tr, height);
+  // helper: transition Left
+  stroke(0,0,255);
+  line(tl, 0, tl, height);
+
+  stroke(0);
+  text(`tr: ${tr.toPrecision(6)}`, 10, height - 10);
+  text(`tl: ${tl.toPrecision(6)}`, 10, height - 30);
+  text(`cS: ${-cS.toPrecision(6)}`, 10, height - 50);
+  text(`lineIndex: ${lineIndex}/${processedLines.length - 2}`, 10, height - 70);
+  pop();
+
+  // console.log(`current horizontal shift: ${cS} | vertical shift: ${vS} | current ws: ${processedLines[lineIndex + 1].ws}, * char width: ${transition.toPrecision(6)}`);
+
+  const lastStep = lineIndex === processedLines.length - 2 ?  true : false;
+  // console.log(`last step: ${lastStep}`);
+
+  // moving forward, if we are beyond the right line (and also the left)
+  if (tl < halfWidth && tr <= halfWidth) {
+    if (lineIndex < processedLines.length - 2) {
+      lineIndex = lineIndex + 1;
+      transitionLeft = transitionRight;
+      transitionRight = processedLines[lineIndex + 1].ws * charWidth;
+    } else if (lastStep) {
+      lineIndex = lineIndex + 1;
     }
+    // console.log(`transition!, tr R: ${transitionRight}, tr L ${transitionLeft} | current shift ${cS} | l index: ${lineIndex}`);
+  }
+
+  // moving backward, if we are beyond the left line (and also the right)
+  if (tl > halfWidth && tr > halfWidth) {
+    lineIndex = Math.max(lineIndex - 1, 0);
+    transitionRight = transitionLeft;
+    transitionLeft = processedLines[lineIndex].ws * charWidth;
+    // console.log(`transition!, tr R: ${transitionRight}, tr L ${transitionLeft} | current shift ${cS} | l index: ${lineIndex}`);
   }
 
   translate(cS, 0);
-  for (let j = 0; j < line.length; j++) {
-    text(line[j], margins + charWidth*j, h + vS);
+  for (let j = 0; j < l.length; j++) {
+    text(l[j], margins + charWidth*j, h + vS);
   }
   pop();
 }
@@ -145,7 +190,7 @@ function getCurrentLines(i, nLines) {
 
 
 function prepareLines() {
-  const pLines = [];
+  const pLines = []; // will contain objects: {"ws": minimum leading whitespace, "l": array of lines}
   let lIndex = -1; // trick to make sure the first line is properly handled
   let cLine;
 
@@ -159,26 +204,35 @@ function prepareLines() {
 
     lIndex = lIndex + 1;
     cLine = lines[lIndex];
-    const whiteSpaceToCrop = cLine.search(/\S|$/);
 
     // CASE: two-lines split: the main line is the basis for cropping
     if (cLine[cLine.length - 1] === "¬") {
+
+      // console.log(`case ¬, line ${cLine}`);
+
       pLines.push({
-        "ws": whiteSpaceToCrop,
+        "ws": cLine.search(/\S|$/) + 2,
         "l": getCurrentLines(lIndex, 2)
       });
 
     // CASE: three-lines split: the previous line is the basis for cropping
     } else if (cLine[cLine.length - 1] === "|") {
+
+      // console.log(`case |, line ${cLine}`);
+
       pLines.push({
-        "ws": whiteSpaceToCrop,
+        "ws": lines[lIndex - 1].search(/\S|$/) + 2,
         "l": getCurrentLines(lIndex, 3)
       });
 
-    // CASE: all others
+    // CASE: all others: : the main line is the basis for cropping
+    // TODO: is this third branch necessary?
     } else {
+
+      // console.log(`case other, line ${cLine}`);
+
       pLines.push({
-        "ws": whiteSpaceToCrop,
+        "ws": cLine.search(/\S|$/) + 2,
         "l": getCurrentLines(lIndex, 1)
       });
     }
@@ -223,24 +277,39 @@ function mousePressed() {
 }
 
 function mouseReleased() {
-  updateCurrentShift(horizontalShift);
+  currentShift += horizontalShift;
+  currentShift = constrain(currentShift + horizontalShift, longestLength, 0);;
   horizontalShift = 0;
 }
 
 function mouseDragged() {
 
-  horizontalShift = - (initCursor.x - mouseX);
+  // console.log(`mX: ${mouseX}, iC: ${initCursor.x} | cur shift ${currentShift}, cur shift + hor shift: ${currentShift + horizontalShift}, longest length: ${longestLength.toPrecision(6)}`);
 
-  // if we reached the left limit, any move right shifts
-  if (mouseX > initCursor.x && currentShift === 0) {
-    initCursor.x = mouseX;
+  // if we are moving backward
+  if (mouseX > initCursor.x) {
+
+    // if are at the left limit, any move right shifts
+    if (currentShift === 0) {
+      initCursor.x = mouseX;
+    }
+
+  // if we are moving backward
+  } else {
+
+    // if we are at the right limit, stop
+    if (currentShift  === longestLength) {
+      initCursor.x = mouseX;
+      // return;
+    }
+
+    // // if are at the left limit, any move right shifts
+    // if (currentShift === longestLength) {
+    //   initCursor.x = mouseX;
+    // }
   }
 
-  // console.log(`current horizontal shift: ${currentShift} | init: ${initCursor.x}, now: ${mouseX} -> ${horizontalShift}`);
-  // console.log(`current vertical shift: ${verticalShift}`);
-}
+  horizontalShift = - (initCursor.x - mouseX);
 
-function updateCurrentShift(h) {
-  currentShift += horizontalShift;
-  currentShift = Math.min(0, currentShift);
+  // console.log(`horiz shift: ${currentShift} | init: ${initCursor.x}, now: ${mouseX} -> ${horizontalShift} | vert shift: ${verticalShift}`);
 }
