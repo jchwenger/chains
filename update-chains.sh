9caf9113cbcdc241ae098ef09f3fb62a59ecc721
jchwenger <34098722+jchwenger@users.noreply.github.com>
2023-05-01 14:45:45 +0100

    chains | update script, submodule renamed

--- update-chains.sh
+++ update-chains.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "----------------------------------------"
-echo "updating submodule 'chains' in 'sketch/assets/'"
+echo "updating submodule 'chains.writing' in 'sketch/assets/'"
 echo
-cd sketch/assets/chains
-git pull
+cd sketch/assets/chains.writing
+git pull origin master
 cd -
 
 echo
 echo "refreshing file lists"
 # https://askubuntu.com/a/811236
-ls -p sketch/assets/chains/chains  | grep -v / > sketch/assets/filenames.english.txt
-ls -p sketch/assets/chains/chaînes | grep -v / > sketch/assets/filenames.french.txt
+ls -p sketch/assets/chains.writing/chains  | grep -v / > sketch/assets/filenames.english.txt
+ls -p sketch/assets/chains.writing/chaînes | grep -v / > sketch/assets/filenames.french.txt
 
 echo "----------------------------------------"
 echo "Commit changes? [Y/n] "
@@ -19,7 +19,7 @@ read answer
 echo "Your answer: $answer"
 if [[ "${answer,,}" =~ ^y*$  ]]
 then
-  git add "sketch/assets/chains"
+  git add "sketch/assets/chains.writing"
   git add "sketch/assets/filenames.english.txt"
   git add "sketch/assets/filenames.french.txt"
   git status
@@ -30,7 +30,7 @@ then
   then
     git commit -m "chains | submodule updated"
   else
-    git restore --staged "sketch/assets/chains"
+    git restore --staged "sketch/assets/chains.writing"
     git restore --staged "sketch/assets/filenames.english.txt"
     git restore --staged "sketch/assets/filenames.french.txt"
     echo "---------"
